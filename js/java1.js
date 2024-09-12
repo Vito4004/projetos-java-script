a1f685b0b9a905ab80c01051dd4e8e0a73652c7c
Vito4004 <163062950+Vito4004@users.noreply.github.com>
2024-09-12 20:53:14 -0300

    Add files via upload

---
+++ js/java1.js
@@ -0,0 +1,11 @@
+function acao(){
+
+let nome =prompt("ola seja bem vindo ,qual é su nome")
+
+alert("ola "+nome+" seja bem vindo meu site")
+
+document.getElementById("paragrafo").innerHTML='ola '+nome+" bem vindo ao meu site pessoal";
+
+
+
+}
---
+++ js/java4.js
@@ -0,0 +1,22 @@
+function validacao(){
+
+    var tell = document.getElementById("tell").value;
+
+
+if(tell =! 4002){
+alert("algo deu errado")
+
+}
+else if(email =! 4002){
+    alert("algo deu errado")
+    
+    }
+    else if(fName =! 4002){
+        alert("algo deu errado")
+        
+        }
+        else if(lName =! 4002){
+            alert("algo deu errado")
+            
+            }
+}
