76473f5b739427a2a64161e861cab3b0fe262147
Vito4004 <163062950+Vito4004@users.noreply.github.com>
2024-09-13 13:57:16 -0300

    Add files via upload

--- js/java4.js
+++ js/java4.js
@@ -1,22 +1,35 @@
 function validacao(){
 
-    var tell = document.getElementById("tell").value;
+    let tell = document.getElementById("tell").value;
+    let email =  document.getElementById("email").value;
+    let fName = document.getElementById("fName").value;
+    let lName =document.getElementById("lName").value;
 
+/////////////////////////////////////////////////////////////////
 
-if(tell =! 4002){
-alert("algo deu errado")
-
-}
-else if(email =! 4002){
-    alert("algo deu errado")
+   if(tell != 300){
+    alert("telefone invalido")
     
-    }
-    else if(fName =! 4002){
-        alert("algo deu errado")
-        
-        }
-        else if(lName =! 4002){
-            alert("algo deu errado")
-            
-            }
+   }
+
+   if(email != "20mata@gmail.com"){
+    alert("email invalido")
+    
+   }
+   
+   if(fName != "vitor"){
+    alert("nome errado ou invalido")
+    
+   }
+
+   if(lName != "leandro"){
+    alert("sobrenome errado ou invalido")
+    
+   }
+
+    alert(`seu telefone ${tell}:\nseu email ${email}:\nseu primeiro nome:${fName} \nseu sobrenome:${lName}`);
+
+   //fin
+
 }
+
