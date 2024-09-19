25182affe08be01bc0ebd165aef2b9f64ec95328
Vito4004 <163062950+Vito4004@users.noreply.github.com>
2024-09-19 20:41:46 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+//inportando coisas
+import './app.css'
+
+//crindo a funçao
+function texto(){
+return(
+  <div>
+   <h1>texto teste</h1>
+ <p>paragrafo teste</p>
+ <p>outro paragrafo teste</p>
+<button>Eu sou um botão</button>
+
+
+  </div>
+  
+);
+}
+
+//exportando coisas
+export default texto
